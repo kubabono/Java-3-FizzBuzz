f103b33fc8f54d092e01e8d0ebe5034b97b3417d
Jakub Bąk <kubabak@wp.pl>
2019-03-03 22:29:12 +0100

    Add first file of FizzBuzz exercise

---
+++ Main.java
@@ -0,0 +1,15 @@
+package fizzbuzz;
+
+// JAKUB B¥K
+
+
+public class Main {
+    
+        public static void main(String[] args) {
+        
+		FizzBuzz fizzBuzzShow = new FizzBuzz(1, 100);
+        fizzBuzzShow.makeFizzBuzz();
+  
+    }
+        
+}
